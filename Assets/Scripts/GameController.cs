43abdcfd0f24e3a23d2bb4064d56a4d96917f2ff
Carlos Tontaquimba <70241275+CarlosTontaquimba@users.noreply.github.com>
2021-02-04 13:59:47 -0500

    Animations
    Animations

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public float parallaxSpeed = 0.18f;
+    public RawImage platform, background;
+    public enum GameState{Idle,Playing};
+    public GameState gameState = GameState.Idle;
+    public GameObject uiIdle;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,21 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))
+        {
+            gameState = GameState.Playing;
+            //Desactiva u oculta el texto si inicia el juego
+            uiIdle.SetActive(false);
+            player.SendMessage("UpdateState","PlayerRun");
+        }else if (gameState == GameState.Playing)
+        {
+            Parallax();
+        }
+    }
+    public void Parallax()
+    {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed , 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4 , 0f, 1f, 1f);
     }
 }
